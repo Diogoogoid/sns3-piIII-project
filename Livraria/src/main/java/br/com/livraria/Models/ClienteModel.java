9e007ccd5df5809fb08cf5b977567b5d34b591f3
Bruno Fonseca <bruuno.foonseca@hotmail.com>
2017-11-07 22:54:29 -0200

    Cliente Model e DAO

--- Livraria/src/main/java/br/com/livraria/Models/ClienteModel.java
+++ Livraria/src/main/java/br/com/livraria/Models/ClienteModel.java
@@ -5,10 +5,166 @@
  */
 package br.com.livraria.Models;
 
+import java.util.Date;
+
 /**
  *
  * @author bruno.falmeida
  */
 public class ClienteModel {
-    
+    //Atributos de dados pessoais
+    private Integer id;
+    private String nomeCliente;
+    private String sexo;
+    private Date dataNasc;
+    private String estadoCivil;
+    private String cpf;
+    private String telefone;
+    private String celular;
+    private String email;
+    private boolean ativo;
+    //Atributo de endereço
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String cep;
+    private String bairro;
+    private String estado;
+    private String cidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nomeCliente;
+    }
+
+    public void setNome(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
 }
